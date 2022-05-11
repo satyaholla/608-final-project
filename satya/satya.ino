d8e77067081588787432e51b2d1c29360cec3555
Satya Holla <sgholla@mit.edu>
2022-05-10 21:53:35 -0400

    fixed server stuff w tokens

--- satya/satya.ino
+++ satya/satya.ino
@@ -55,7 +55,7 @@ using namespace std;
   //const char PASSWORD[] = "";
   //
   //
-  const char NETWORK[] = "EECS_Labs";
+  const char NETWORK[] = "MIT";
   const char PASSWORD[] = "";
 
   /* Having network issues since there are 50 MIT and MIT_GUEST networks?. Do the following:
@@ -116,36 +116,6 @@ using namespace std;
 
   char* SERVER = "googleapis.com";  // Server URL
 
-  void do_http_request(char* host, char* request, char* response, uint16_t response_size, uint16_t response_timeout, uint8_t serial){
-    if (client2.connect(host, 80)) { //try to connect to host on port 80
-      if (serial) Serial.print(request);//Can do one-line if statements in C without curly braces
-      client2.print(request);
-      memset(response, 0, response_size); //Null out (0 is the value of the null terminator '\0') entire buffer
-      uint32_t count = millis();
-      while (client2.connected()) { //while we remain connected read out data coming back
-        client2.readBytesUntil('\n',response,response_size);
-        if (serial) Serial.println(response);
-        if (strcmp(response,"\r")==0) { //found a blank line!
-          break;
-        }
-        memset(response, 0, response_size);
-        if (millis()-count>response_timeout) break;
-      }
-      memset(response, 0, response_size);  
-      count = millis();
-      while (client2.available()) { //read out remaining text (body of response)
-        char_append(response,client2.read(), OUT_BUFFER_SIZE);
-      }
-      if (serial) Serial.println(response);
-      client2.stop();
-      if (serial) Serial.println("-----------");  
-    }else{
-      if (serial) Serial.println("connection failed :/");
-      if (serial) Serial.println("wait 0.5 sec...");
-      client2.stop();
-    }
-  }  
-
   uint32_t timer;
 
   struct personKey {
@@ -174,9 +144,9 @@ using namespace std;
     uint32_t key_exchange_timer;
     uint32_t last_get_request;
     uint32_t wait_timer;
-    const uint16_t KEY_EXCHANGE_TIMEOUT = 10000;
-    const uint16_t GET_REQUEST_PERIOD = 100;
-    const uint16_t WAIT_TIME = 500;
+    const static uint16_t KEY_EXCHANGE_TIMEOUT = 10000;
+    const static uint16_t GET_REQUEST_PERIOD = 100;
+    const static uint16_t WAIT_TIME = 500;
     personKey pk;
     
 
@@ -221,7 +191,7 @@ using namespace std;
         switch (state) {
             case GET_P_AND_G:
                 request[0] = '\0';
-                sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py?other_user=%s&this_user=%s&get_type=gen_exchange HTTP/1.1\r\n", other_user, this_user);
+                sprintf(request, "GET http://608dev-2.net/sandbox/sc/team33/final/key_exchange.py?other_user=%s&this_user=%s&get_type=gen_exchange HTTP/1.1\r\n", other_user, this_user);
                 strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
                 strcat(request, "\r\n");
                 do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
@@ -253,7 +223,7 @@ using namespace std;
                     char body[200];
                     sprintf(body, "post_type=%s&other_user=%s&this_user=%s&exponent=%llu", "exp_exchange", other_user, this_user, g_a); //generate body, posting temp, humidity to server
                     int body_len = strlen(body); //calculate body length (for header reporting)
-                    sprintf(request, "POST http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py HTTP/1.1\r\n");
+                    sprintf(request, "POST http://608dev-2.net/sandbox/sc/team33/final/key_exchange.py HTTP/1.1\r\n");
                     strcat(request, "Host: 608dev-2.net\r\n");
                     strcat(request, "Content-Type: application/x-www-form-urlencoded\r\n");
                     sprintf(request + strlen(request), "Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
@@ -270,7 +240,7 @@ using namespace std;
                 if (millis() - key_exchange_timer < KEY_EXCHANGE_TIMEOUT) {
                     if (millis() - last_get_request > GET_REQUEST_PERIOD) {
                       request[0] = '\0';
-                      sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py?other_user=%s&this_user=%s&get_type=exp_exchange HTTP/1.1\r\n", other_user, this_user);
+                      sprintf(request, "GET http://608dev-2.net/sandbox/sc/team33/final/key_exchange.py?other_user=%s&this_user=%s&get_type=exp_exchange HTTP/1.1\r\n", other_user, this_user);
                       strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
                       strcat(request, "\r\n");
                       do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
@@ -307,7 +277,7 @@ using namespace std;
 
   class MessageHandler {
     private:
-    const uint16_t MESSAGE_HANDLING_PERIOD = 500;
+    const static uint16_t MESSAGE_HANDLING_PERIOD = 500;
     uint32_t last_message_check;
     const char* this_user;
 
@@ -330,7 +300,7 @@ using namespace std;
     int update() {
       if (millis() - last_message_check > MESSAGE_HANDLING_PERIOD) {
         request[0] = '\0';
-        sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/message.py?recipient=%s HTTP/1.1\r\n", this_user);
+        sprintf(request, "GET http://608dev-2.net/sandbox/sc/team33/final/messages.py?token=12345&recipient=%s HTTP/1.1\r\n", this_user);
         strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
         strcat(request, "\r\n");
         do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
@@ -355,7 +325,7 @@ using namespace std;
 
   class NewKeyExchangeHandler {
     private:
-    const uint16_t KEY_EXCHANGE_CHECK_PERIOD = 500;
+    const static uint16_t KEY_EXCHANGE_CHECK_PERIOD = 500;
     uint32_t last_key_exchange_check;
     std::vector<KeyExchanger> exchangers;
     const char* this_user;
@@ -371,7 +341,7 @@ using namespace std;
     int update() {
       if (millis() - last_key_exchange_check > KEY_EXCHANGE_CHECK_PERIOD) {
         request[0] = '\0';
-        sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py?recipient=%s&get_type=new_key_exchange HTTP/1.1\r\n", this_user);
+        sprintf(request, "GET http://608dev-2.net/sandbox/sc/team33/final/key_exchange.py?recipient=%s&get_type=new_key_exchange HTTP/1.1\r\n", this_user);
         strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
         strcat(request, "\r\n");
         do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
@@ -394,7 +364,7 @@ using namespace std;
   Button button3(45);
   NewKeyExchangeHandler key_exchange_handler(USERNAME);
   MessageHandler message_handler(USERNAME);
-  KeyExchanger* key_exchanger_pointer = nullptr;
+  KeyExchanger* key_exchanger_pointer = NULL;
 
 
 void setup() {
@@ -514,13 +484,13 @@ void loop() {
       Serial.println("Doing key exchange");
       switch (key_exchanger_pointer->update()) {
         case -1:
-          delete key_exchange_pointer;
-          key_exchanger_pointer = nullptr;
+          delete key_exchanger_pointer;
+          key_exchanger_pointer = NULL;
           main_state = 0;
           break;
         case 1:
-          delete key_exchange_pointer;
-          key_exchanger_pointer = nullptr;
+          delete key_exchanger_pointer;
+          key_exchanger_pointer = NULL;
           main_state = 1;
           break;
       };
@@ -570,9 +540,9 @@ void loop() {
 
 void send_message(const char* user, const char* message, bigNum key) {
   char body[200];
-  sprintf(body, "sender=%s&recipient=%s&message=(%s, %llu)", USERNAME, user, message, key); //generate body, posting temp, humidity to server
+  sprintf(body, "token=12345&sender=%s&recipient=%s&message=(%s, %llu)", USERNAME, user, message, key); //generate body, posting temp, humidity to server
   int body_len = strlen(body); //calculate body length (for header reporting)
-  sprintf(request, "POST http://608dev-2.net/sandbox/sc/sgholla/final-project/message.py HTTP/1.1\r\n");
+  sprintf(request, "POST http://608dev-2.net/sandbox/sc/sgholla/final-project/messages.py HTTP/1.1\r\n");
   strcat(request, "Host: 608dev-2.net\r\n");
   strcat(request, "Content-Type: application/x-www-form-urlencoded\r\n");
   sprintf(request + strlen(request), "Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
@@ -580,114 +550,4 @@ void send_message(const char* user, const char* message, bigNum key) {
   strcat(request, body); //body
   strcat(request, "\r\n"); //new line
   do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-  message_send_state = 1;
 }
-
-// void message_handler() {
-//   if (millis() - last_message_check > MESSAGE_HANDLING_TIMEOUT) {
-//     request[0] = '\0';
-//     sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/message.py?recipient=%s HTTP/1.1\r\n", USERNAME);
-//     strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
-//     strcat(request, "\r\n");
-//     do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-//     Serial.println(response);
-//     if (response[0] != '^') {
-//       new_messages.push_back(SString(response));
-//     }
-//     last_message_check = millis();
-//   }
-// }
-
-// void key_exchange(const char* user) { // TODO: make this a class update function
-//   bigNum g_a;
-//   personKey pk;
-
-//   switch (key_exchange_state) {
-//     case 0:
-//       request[0] = '\0';
-//       sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py?other_user=%s&this_user=%s&get_type=gen_exchange HTTP/1.1\r\n", user, USERNAME);
-//       strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
-//       strcat(request, "\r\n");
-//       do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-//       {
-//         char response_copy[OUT_BUFFER_SIZE];
-//         strcpy(response_copy, response);
-//         int comma_ind = -1;
-//         while (response_copy[++comma_ind] != ',');
-//         response_copy[comma_ind] = '\0';
-//         pk.prime = atoi(response_copy);
-//         pk.generator = atoi(response_copy + comma_ind + 1);
-//       }
-
-//       g_a = set_a(pk.prime, pk.generator, pk);
-
-//       // POST THE G_A
-//       {
-//         char body[200];
-//         sprintf(body, "post_type=%s&other_user=%s&this_user=%s&exponent=%llu", "exp_exchange", user, USERNAME, g_a); //generate body, posting temp, humidity to server
-//         int body_len = strlen(body); //calculate body length (for header reporting)
-//         sprintf(request, "POST http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py HTTP/1.1\r\n");
-//         strcat(request, "Host: 608dev-2.net\r\n");
-//         strcat(request, "Content-Type: application/x-www-form-urlencoded\r\n");
-//         sprintf(request + strlen(request), "Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
-//         strcat(request, "\r\n"); //new line from header to body
-//         strcat(request, body); //body
-//         strcat(request, "\r\n"); //new line
-//         do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-//       }
-
-//       key_exchange_state = 1;
-//       key_exchange_timer = millis();
-//       break;
-//     case 1:
-//       if (millis() - key_exchange_timer < KEY_EXCHANGE_TIMEOUT) {
-//         if (millis() - last_get_request > 100) {
-//           request[0] = '\0';
-//           sprintf(request, "GET http://608dev-2.net/sandbox/sc/sgholla/final-project/key_exchange.py?other_user=%s&this_user=%s&get_type=exp_exchange HTTP/1.1\r\n", user, USERNAME);
-//           strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
-//           strcat(request, "\r\n");
-//           do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-//           if (response[0] != 'x') {
-//             Serial.println("REACHED HERE");
-//             pk.g_b = atoi(response);
-//             key_exchange_state = 2;
-//           } else {
-//             Serial.println("Response is not yet i guesss");
-//           }
-//           last_get_request = millis();
-//         }
-//       }
-//       else {
-//         Serial.println("Exponent exchange failed, trying again.");
-//         key_exchange_state = 0;
-//       }
-//       break;
-//     case 2:
-//       pk.key = largePow(pk.prime, pk.g_b, pk.a);
-//       Serial.println("Done");
-//       key_exchange_state = 3;
-//       break;
-//     case 3:
-//       keys.push_back(pk);
-//       break;
-//   }
-
-// }
-
-// bigNum set_a(const bigNum &p, const bigNum &g, personKey pk) {
-//   pk.a = rand() % p;
-//   return largePow(p, g, pk.a);
-// }
-
-// bigNum largePow(const bigNum &p, bigNum g, bigNum a) {
-//   bigNum res = 1;
-//   while (a) {
-//     if (a % 2) res *= g;
-
-//     a >>= 1;
-//     g *= g;
-//     g %= p;
-//     res %= p;
-//   }
-//   return res;
-// }
--- satya/support_functions.ino
+++ satya/support_functions.ino
@@ -59,7 +59,35 @@ uint8_t char_append(char* buff, char c, uint16_t buff_size) {
  * Return value:
  *    void (none)
  */
-
+void do_http_request(char* host, char* request, char* response, uint16_t response_size, uint16_t response_timeout, uint8_t serial){
+  if (client2.connect(host, 80)) { //try to connect to host on port 80
+    if (serial) Serial.print(request);//Can do one-line if statements in C without curly braces
+    client2.print(request);
+    memset(response, 0, response_size); //Null out (0 is the value of the null terminator '\0') entire buffer
+    uint32_t count = millis();
+    while (client2.connected()) { //while we remain connected read out data coming back
+      client2.readBytesUntil('\n',response,response_size);
+      if (serial) Serial.println(response);
+      if (strcmp(response,"\r")==0) { //found a blank line!
+        break;
+      }
+      memset(response, 0, response_size);
+      if (millis()-count>response_timeout) break;
+    }
+    memset(response, 0, response_size);  
+    count = millis();
+    while (client2.available()) { //read out remaining text (body of response)
+      char_append(response,client2.read(), OUT_BUFFER_SIZE);
+    }
+    if (serial) Serial.println(response);
+    client2.stop();
+    if (serial) Serial.println("-----------");  
+  }else{
+    if (serial) Serial.println("connection failed :/");
+    if (serial) Serial.println("wait 0.5 sec...");
+    client2.stop();
+  }
+}  
 
 /*----------------------------------
    do_https_request Function. Similar to do_http_request, but supports https requests
